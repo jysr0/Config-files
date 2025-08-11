54d23c4037df055553c7519c12039f5a9c3d5b9c
ysr <141933879+jysr0@users.noreply.github.com>
2025-08-11 15:43:34 +0100

    Add files via upload

---
+++ env-vars.sh
@@ -0,0 +1,29 @@
+export DISPLAY=":0"
+
+export HOME="$HOME"
+export PATH="$PATH"
+
+#SYSTEM WIDE ENV VARIABLES:
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_STATE_HOME="$HOME/.local/state"
+#export XDG_RUNTIME_DIR="$HOME/.runtime.dir"
+export XDG_CONFIG_DIRS="/etc/xdg"
+export XDG_DATA_DIRS="/usr/share:/usr/local/share"
+export XDG_CURRENT_DESKTOP="dwm"
+export XDG_SESSION_TYPE="x11"
+
+#USER XDG ENV VAIRIABLES:
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_VIDEOS_DIR="$HOME/Videos"
+#export XDG_TEMPLATES_DIR="$HOME/"
+#export XDG_PUBLICSHARE_DIR="$HOME/"
+export EDITOR="vim"
+export BROWSER="librewolf"
+export TERM="alacritty"
+
